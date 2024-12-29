9ee9c0cfb1e4f6ab197a3b4ba2de87d878268f58
Mr X <khatrirajesh1003@gmail.com>
2024-12-29 16:45:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "27161113"))
+API_HASH = getenv("API_HASH", "65c4b51f54049ebe22bc01fd9a95975b")
+BOT_TOKEN = getenv("BOT_TOKEN", "8058972609:AAE7TwlqxpME8FxnbvJoD4K7BWMqe3-H4vU")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6401497985").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+LOG_GROUP = getenv("LOG_GROUP", "-1002366201837")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002374060756"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002374060756"))
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")
